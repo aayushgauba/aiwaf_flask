2bea0b239af5b56691346be8ade5d3028ff68396
Aayush Gauba <55630226+aayushgauba@users.noreply.github.com>
2026-01-21 16:49:16 -0600

    Version Upgrade

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="aiwaf-flask",
-    version="0.1.0",
+    version="0.1.1",
     author="Aayush Gauba",
     author_email="gauba.aayush@gmail.com",
     description="Advanced AI-powered Web Application Firewall for Flask with intelligent threat detection, rate limiting, IP blocking, and real-time protection against web attacks",
